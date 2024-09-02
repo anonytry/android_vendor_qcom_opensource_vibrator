d3105a9340ea22333180f5b05e10aa35cd9e417a
V S Ganga VaraPrasad (VARA) Adabala <quic_vadabala@quicinc.com>
2024-09-02 21:09:49 +0530

    Revert "vibratorCL: Trigger dynamic calib based on timer"
    This reverts commit cb68efca33da51532739bc46c306c78913fb7ecb.
    
    Change-Id: I98af1ec4384d53011da0259928c52f87a184096e

--- aidl/VibratorCL/Vibrator.h
+++ aidl/VibratorCL/Vibrator.h
@@ -55,7 +55,6 @@ public:
     static std::condition_variable cv;
     static std::condition_variable Eventcv;
     static std::thread OffThread;
-	static std::atomic<bool> CalThrdCreated;
 
     static bool OffThrdCreated;
     static bool ActiveUsecase;
@@ -63,7 +62,6 @@ public:
     int on(int32_t timeoutMs);
     void offEffect();
     void HapticsWait();
-	void HapticsCalibThread();
     void HapticsWaitTillWaveformComp();
     int32_t StopHapticsStream();
     int32_t offCurrentEffect();
